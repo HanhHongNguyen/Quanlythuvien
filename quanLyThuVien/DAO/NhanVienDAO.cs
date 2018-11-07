a1d29b586a88d78987ddcd01cfe83964bf9360dc
lathihongphuong <hongphuong.hp9898@gmail.com>
2018-11-07 23:25:47 +0700

    hien thi dsnv

---
+++ quanLyThuVien/DAO/NhanVienDAO.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using DTO;
+namespace DAO
+{
+    public class NhanVienDAO : DataProvider
+    {
+        public List<NhanVien> getNV()
+        {
+            string sql = "SELECT * FROM NhanVien";
+            string id, name, sdt;
+            List<NhanVien> list = new List<NhanVien>();
+            Connect();
+
+            try
+            {
+                SqlDataReader dr = myExecuteReader(sql);
+                while (dr.Read())
+                {
+                    id = dr[0].ToString();
+                    name = dr[1].ToString();
+                    sdt = dr[2].ToString();
+
+                    NhanVien nv = new NhanVien(id, name, sdt);
+                    list.Add(nv);
+                }
+                dr.Close();
+                return list;
+            }
+            catch (SqlException ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                Disconnect();
+            }
+        }
+    }
+}
---
+++ quanLyThuVien/DTO/NhanVien.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DTO
+{
+    public class NhanVien
+    {
+        public string IDNhanVien { get; set; }
+        public string TenNV { get; set; }
+        public string SDT { get; set; }
+        public NhanVien(string id, string name, string phone)
+        {
+            this.IDNhanVien = id;
+            this.TenNV = name;
+            this.SDT = phone;
+        }
+    }
+}
