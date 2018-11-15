9d53358651a0d406347376925af2054233d510ae
hanh nguyen <1651010054hanh@ou.edu.vn>
2018-11-15 10:02:15 +0700

    fix

--- quanLyThuVien/DAO/DocGiaDAO.cs
+++ quanLyThuVien/DAO/DocGiaDAO.cs
@@ -47,6 +47,27 @@ namespace DAO
             
         }
 
+        public bool UpdateDG(DocGia docGia)
+        {
+            string sql = "UPDATE DocGia SET TenDocGia = @name, DiaChi = @address, SDT = @phone, Email = @email Where MaDocGia = @id";
+            List<SqlParameter> Parameters = new List<SqlParameter>();
+            Parameters.Add(new SqlParameter("@id", docGia.MaDG));
+            Parameters.Add(new SqlParameter("@name", docGia.TenDG));
+            Parameters.Add(new SqlParameter("@address", docGia.DiaChi));
+            Parameters.Add(new SqlParameter("@phone", docGia.SDT));
+            Parameters.Add(new SqlParameter("@email", docGia.Email));
+            try
+            {
+                return myExecuteNonQuery(sql, CommandType.Text, Parameters) > 0;
+
+            }
+            catch (SqlException ex)
+            {
+
+                throw ex;
+            }
+        }
+
         public int Add (DocGia docGia)
         {
             string sql = "INSERT INTO DocGia VALUES (@id,@name,@address,@phone,@email)";
@@ -83,21 +104,6 @@ namespace DAO
                 throw ex;
             }
         }
-        //public int UpdateDG(DocGia dg)
-        //{
-
-        //    string sql = "UPDATE Sinhvien SET MaDocGia='" + dg.MaDG + "', TenDocGia = N'" + dg.TenDG + "', DiaChi= N'" + dg.DiaChi + "', SDT= N'" + dg.SDT + "',Email= N'" + dg.Email + "' where MaDocGia = '" + dg.MaDG + "'";
-
-        //    try
-        //    {
-        //        int sohang = myExcuteNonQuery(sql, CommandType.Text, Parameters);
-        //        return sohang;
-        //    }
-        //    catch (SqlException ex)
-        //    {
-
-        //        throw ex;
-        //    }
-        //}
+        
     }
 }
