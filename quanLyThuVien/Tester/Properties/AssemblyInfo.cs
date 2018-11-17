e822d6192d8ce2fcd5c11003c23660662c35828d
hanh nguyen <1651010054hanh@ou.edu.vn>
2018-11-17 17:29:09 +0700

    Tester

---
+++ quanLyThuVien/Tester/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tester")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tester")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8654756d-f835-49fd-94d5-aaddb3a360e6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ quanLyThuVien/Tester/UnitTest1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DAO;
+using BUS;
+using DTO;
+
+namespace Tester2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private NhanVienDAO nvDAO = new NhanVienDAO();
+        private NhanVien nv;
+        private DocGiaDAO dgDAO = new DocGiaDAO();
+        private DocGia dg;
+        private SachDAO sachDAO = new SachDAO();
+        private Sach s;
+        private TheLoaiDAO tlDAO = new TheLoaiDAO();
+        private TheLoai tl;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            this.nv = new NhanVien("NV20", "Hu?nh Th? Lan", "0904356578");
+            this.dg = new DocGia("DG19", "Nguy?n Van A", "371 Nguy?n Ki?m", "0125487282", "");
+            this.s = new Sach("SA11", "Hoa h?ng den", "TG04", "TL03", 1965, "M?i");
+            this.tl = new TheLoai("TL17", "M? và bé");
+        }
+
+        [TestMethod]
+        public void ThemDocGia()
+        {
+            int dem = this.dgDAO.getDocGia().Count;
+            if (this.dgDAO.Add(dg) == 1)
+            {
+                dem += 1;
+            }
+
+            Assert.AreEqual(dem, this.dgDAO.getDocGia().Count);
+
+        }
+
+        [TestMethod]
+        public void ThemNhanVien()
+        {
+            int dem = this.nvDAO.getNV().Count;
+            if (this.nvDAO.Add(nv) == 1)
+            {
+                dem += 1;
+            }
+
+            Assert.AreEqual(dem, this.nvDAO.getNV().Count);
+        }
+
+        [TestMethod]
+        public void ThemSach()
+        {
+            int dem = this.sachDAO.getSach().Count;
+            if (this.sachDAO.Add(s) == 1)
+            {
+                dem += 1;
+            }
+
+            Assert.AreEqual(dem, this.sachDAO.getSach().Count);
+        }
+
+        [TestMethod]
+        public void ThemTheLoai()
+        {
+            int dem = this.tlDAO.getTL().Count;
+            if (this.tlDAO.Add(tl) == 1)
+            {
+                dem += 1;
+            }
+
+            Assert.AreEqual(dem, this.tlDAO.getTL().Count);
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
